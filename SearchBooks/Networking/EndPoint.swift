cabb1c55348735ff24cc7bb746149414afd7cbe1
maikytech <maikytech1307@gmail.com>
2022-02-07 22:05:50 -0500

    Set NetworkingProvider

---
+++ SearchBooks/Networking/EndPoint.swift
@@ -0,0 +1,13 @@
+//
+//  EndPoint.swift
+//  SearchBooks
+//
+//  Created by Maiqui Cedeño on 7/02/22.
+//
+
+import Foundation
+
+struct EndPoint {
+    static let domain = "https://api.itbook.store/1.0/"
+    static let search = "search/"
+}
--- SearchBooks/Networking/NetworkingProvider.swift
+++ SearchBooks/Networking/NetworkingProvider.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+final class NetworkingProvider {
+    
+    //MARK: - Variables
+    static let shared = NetworkingProvider()
+    
+    func fetchData(technology: String, success: @escaping (_ library: Library) -> (), failure: @escaping (_ error: Error) -> ()) {
+        let url: String = "\(EndPoint.domain)\(EndPoint.search)"
+        
+        guard let objectUrl = URL(string: url) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: objectUrl) { data, response, error in
+            guard let data = data else {
+                print(error.debugDescription)
+                return
+            }
+            
+            do {
+                let decoder = try JSONDecoder().decode(Library.self, from: data)
+                success(decoder)
+            } catch let error {
+                failure (error)
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
